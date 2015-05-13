9a44d713d7772b8295f844ca623a3c512338cbe1
R912COM <R912COM@R912COM-PC>
2015-05-13 13:07:06 +0900

    chapter 5 skimming finish

--- JavaAWTpractice/src/TestPanel.java
+++ JavaAWTpractice/src/TestPanel.java
@@ -7,6 +7,7 @@ public class TestPanel extends Frame {
 	}
 
 	public static void main(String[] args) {
+		/*
 		TestPanel fr = new TestPanel("Frame with Panel");
 		Panel pan = new Panel();
 		fr.setSize(200, 200);
@@ -19,6 +20,48 @@ public class TestPanel extends Frame {
 															// button
 		fr.addWindowListener(listener);
 		fr.setVisible(true);
+		*/
+		TestPanel f = new TestPanel("Simple FlowLayout Example");
+	      /*
+	       f.setLayout(new FlowLayout());
+	      Button b1 = new Button("Press Me");
+	      Button b2 = new Button("Don't Press Me");
+	      f.add(b1);
+	      f.add(b2);
+	      f.pack();
+	      WindowDestroyer listener = new WindowDestroyer();  // window destroy button
+	       f.addWindowListener(listener);
+	      f.setVisible(true);
+	      *//*
+		 f.setLayout(new BorderLayout());
+	     Button bn = new Button("Bn");
+	     Button bs = new Button("Bs");
+	     Button be = new Button("Be");
+	     Button bw = new Button("Bw");
+	     Button bc = new Button("Bc");
+	     f.add(bn, BorderLayout.NORTH);
+	     f.add(bs, BorderLayout.SOUTH);
+	     f.add(be, BorderLayout.EAST);
+	     f.add(bw, BorderLayout.WEST);
+	     f.add(bc, BorderLayout.CENTER);
+	     f.setSize(200,200);
+	     //f.pack();
+	     WindowDestroyer listener = new WindowDestroyer();  
+	        f.addWindowListener(listener);
+	     f.setVisible(true);
+	     */
+		f.setLayout(new GridLayout(3,2));
+	    Button b1 = new Button("B1");
+	    Button b2 = new Button("B2");
+	    Button b3 = new Button("B3");
+	    Button b4 = new Button("B4");
+	    Button b5 = new Button("B5");
+	    Button b6 = new Button("B6");
+	    f.add(b1);  f.add(b2); f.add(b3); f.add(b4); f.add(b5); f.add(b6); f.pack();
+
+	    WindowDestroyer listener = new WindowDestroyer();  
+	      f.addWindowListener(listener);
+	    f.setVisible(true);
 
 	}
 }
